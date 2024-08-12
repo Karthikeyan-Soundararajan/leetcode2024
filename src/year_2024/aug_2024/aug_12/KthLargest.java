40ae85e673e20437e175a8d652f293f3bfc9261f
karthi29011992 <9894290119k@gmail.com>
2024-08-12 09:03:30 +0530

    Aug 12th Solution

--- src/year_2024/aug_2024/aug_12/KthLargest.java
+++ src/year_2024/aug_2024/aug_12/KthLargest.java
@@ -1,16 +1,36 @@
 package year_2024.aug_2024.aug_12;
 
+import java.util.PriorityQueue;
+
 public class KthLargest {
 
-    public KthLargest(int k, int[] nums) {
+    int k;
+    PriorityQueue<Integer> numbers;
 
+    public KthLargest(int k, int[] nums) {
+        this.k = k;
+        numbers = new PriorityQueue<>(k);
+        for (int num : nums) {
+            add(num);
+        }
     }
 
     public int add(int val) {
-        return val;
+        if (numbers.size()<k){
+            numbers.add(val);
+        } else if (val > numbers.peek() ) {
+            numbers.poll();
+            numbers.offer(val);
+        }
+        return numbers.peek();
     }
 
     public static void main(String[] args) {
-
+        KthLargest kthLargest = new KthLargest(3,new int[]{4,5,8,2});
+        System.out.println(kthLargest.add(3));
+        System.out.println(kthLargest.add(5));
+        System.out.println(kthLargest.add(10));
+        System.out.println(kthLargest.add(9));
+        System.out.println(kthLargest.add(4));
     }
 }
