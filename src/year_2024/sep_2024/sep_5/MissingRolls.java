b50a7e7191c1fb7fbb3462f141136cbe1bed015f
karthi29011992 <9894290119k@gmail.com>
2024-09-05 08:15:47 +0530

    Sep 5th Solution

---
+++ src/year_2024/sep_2024/sep_5/MissingRolls.java
@@ -0,0 +1,47 @@
+package year_2024.sep_2024.sep_5;
+
+import java.util.Arrays;
+
+public class MissingRolls {
+
+    public int[] missingRolls(int[] rolls, int mean, int n) {
+        int nm = n+rolls.length;
+        int availableSum = 0;
+        for (int i = 0; i < rolls.length; i++) {
+            availableSum += rolls[i];
+        }
+        int requiredSum = (mean * nm) - availableSum;
+        double requireMean =(double) requiredSum/n;
+        if (requireMean>6 || requireMean < 1){
+            return new int[]{};
+        }
+        int[] result = new int[n];
+        if(requireMean % 1 == 0){
+            Arrays.fill(result, (int) requireMean);
+        }else {
+            int i = 0;
+            while (requiredSum!=0){
+                double curr = Math.round(requireMean);
+                result[i++] = (int) curr;
+                requiredSum -= curr;
+                n--;
+                requireMean = (double) requiredSum / n;
+                if(requireMean % 1 == 0) {
+                    Arrays.fill(result,i,result.length,(int) requireMean);
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        MissingRolls missingRolls = new MissingRolls();
+//        int [] res = missingRolls.missingRolls(new int[]{3,2,4,3},4,2);
+        int [] res = missingRolls.missingRolls(new int[]{1,5,6},3,4);
+//        int [] res = missingRolls.missingRolls(new int[]{1,3,2,4},6,4);
+        for (int i = 0; i < res.length; i++) {
+            System.out.println(res[i]);
+        }
+    }
+}
