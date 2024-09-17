a9f6f277a9ec5c5609158f072761a2fefa0760d3
karthi29011992 <9894290119k@gmail.com>
2024-09-17 08:49:02 +0530

    Sep 17th Solution

---
+++ src/year_2024/sep_2024/sep_17/UncommonFromSentences.java
@@ -0,0 +1,18 @@
+package year_2024.sep_2024.sep_17;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class UncommonFromSentences {
+
+    public String[] uncommonFromSentences(String s1, String s2) {
+        s1 = s1+" "+s2;
+        return Arrays.stream(s1.split(" ")).collect(Collectors.groupingBy(s -> s, Collectors.counting())).entrySet().stream().filter(stringLongEntry -> stringLongEntry.getValue()==1).map(stringLongEntry -> stringLongEntry.getKey()).toArray(String[]::new);
+    }
+    public static void main(String[] args) {
+        UncommonFromSentences uncommonFromSentences = new UncommonFromSentences();
+        System.out.println(Arrays.toString(uncommonFromSentences.uncommonFromSentences("this apple is sweet","this apple is sour")));
+    }
+}
